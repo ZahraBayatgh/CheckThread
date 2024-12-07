d2b87f548a2b32fb2e73d0338eede0103f17ea4c
Zahra Bayat <zahra.bayat@vanitech.com.au>
2024-12-07 07:42:57 +0330

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication10.Controllers;
+[Route("api/test")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+
+
+    [HttpGet("thread")]
+    public IActionResult SimulateBlocking()
+    {
+        Thread.Sleep(10000);
+        return Ok("Thread.Sleep simulation completed.");
+    }
+
+    [HttpGet("task")]
+    public async Task<IActionResult> SimulateNonBlocking()
+    {
+        await Task.Delay(10000);
+        return Ok("Task.Delay simulation completed.");
+    }
+}
---
+++ test.js
@@ -0,0 +1,25 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 200,
+    duration: '30s', 
+};
+
+export default function () {
+    const url = __ENV.TEST_URL || 'http://localhost:5000/api/test/task';
+
+
+    const start = Date.now();
+    const res = http.get(url);
+    const end = Date.now();
+
+    console.log(`Request Duration: ${end - start}ms`);
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+
+    sleep(10 - ((end - start) / 1000));
+}
